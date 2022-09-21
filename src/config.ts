0a95bcd1c04a3448bcc76cf8594b06aa55928d8b
René Koller <r.koller@me.com>
2022-09-21 12:52:57 +0200

    Use credentials directly in the config file, without .env for easier management

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ export default {
   spotify: {
     baseUrl: "https://api.spotify.com/v1",
     authUrl: "https://accounts.spotify.com/api/token",
-    clientId: import.meta.env.VITE_SPOTIFY_CLIENT_ID,
-    clientSecret: import.meta.env.VITE_SPOTIFY_CLIENT_SECRET,
+    clientId: "YOUR_CLIENT_ID",
+    clientSecret: "YOUR_CLIENT_SECRET",
   },
 };
